84d089a372e85e0c338f60ab5030dc967166f2fd
Benjamin <dilgopierre@gmail.com>
2024-09-10 14:53:08 +0200

    :truck: Rename table to tables

--- server/src/database/tables.js
+++ server/src/database/tables.js
@@ -1,8 +1,8 @@
 import pool from "./pool.js";
 import { UsersRepository } from "./models/users.repository.js";
 
-const table = {};
+const tables = {};
 
-table.users = new UsersRepository(pool);
+tables.users = new UsersRepository(pool);
 
-export default table;
+export default tables;
