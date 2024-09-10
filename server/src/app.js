3d6cfde15f0c9068bbd9752d079b4c5da019b179
Benjamin <dilgopierre@gmail.com>
2024-09-10 14:46:40 +0200

    :hammer: Use logger and error middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,20 @@
 import express, { Router } from "express";
 import { resolve } from "path";
-import loadRoutes from "./utils/loadRoutes.js";
+import loadRoutes from "./utils/loader/loadRoutes.js";
+import { errorMiddleware } from "./middlewares/error.middleware.js";
+import loggerMiddleware from "./middlewares/logger.middleware.js";
+import bodyParser from "body-parser";
 
 const app = express();
 const apiRouter = Router();
+
+app.use(bodyParser.json());
+app.use(loggerMiddleware);
+
 await loadRoutes(apiRouter, resolve("./src/routes"));
 
 app.use("/api", apiRouter);
+
+app.use(errorMiddleware);
 
 export default app;
