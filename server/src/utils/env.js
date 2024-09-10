b8f51659c87d4581946a315e9c01cba8ca6e93db
Benjamin <dilgopierre@gmail.com>
2024-09-10 13:53:18 +0200

    :hammer: Add env variable to javascript

--- server/src/utils/env.js
+++ server/src/utils/env.js
@@ -3,3 +3,8 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 3000;
+export const DATABASE = process.env.DATABASE || "DATABASE";
+export const DATABASE_HOST = process.env.DATABASE_HOST || "localhost";
+export const DATABASE_PORT = process.env.DATABASE_PORT || 3306;
+export const DATABASE_USER = process.env.DATABASE_USER || "USER";
+export const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || "PASSWORD";
