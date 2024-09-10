6ceb95729d0371a72cf47fe39e910b11c6308528
Benjamin <dilgopierre@gmail.com>
2024-09-10 13:55:17 +0200

    :hammer: Use auto import on api router

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,11 @@
-import express from "express";
+import express, { Router } from "express";
+import { resolve } from "path";
+import loadRoutes from "./utils/loadRoutes.js";
 
 const app = express();
+const apiRouter = Router();
+await loadRoutes(apiRouter, resolve("./src/routes"));
+
+app.use("/api", apiRouter);
 
 export default app;
