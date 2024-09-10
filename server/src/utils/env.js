2ff43a3fbcd760063e2db14dfd977924a5820c7b
Benjamin <dilgopierre@gmail.com>
2024-09-10 14:54:43 +0200

    :wrench: Add secret env variable

--- server/src/utils/env.js
+++ server/src/utils/env.js
@@ -3,6 +3,7 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 3000;
+export const SECRET = process.env.SECRET || "SECRET";
 export const DATABASE = process.env.DATABASE || "DATABASE";
 export const DATABASE_HOST = process.env.DATABASE_HOST || "localhost";
 export const DATABASE_PORT = process.env.DATABASE_PORT || 3306;
